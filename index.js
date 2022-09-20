e9738f20adbff4867c6d26854ee0a5c85e2cc36e
tukaramsawant <tsawant107@gmail.com>
2022-09-20 10:51:22 +0530

    api for user

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose")
-=======
 const express = require('express')
->>>>>>> 15ff24c37acf3022b31b70e020edcea479b25513
+
+
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// connect with db
+mongoose.connect("mongodb+srv://tsawant635:tsawant635@cluster0.r6b5bha.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("successfully connected to db");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/auth");
+const ordersRoutes = require("./routes/ordersRoute");
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/orders", ordersRoutes);
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
