ec660d8fccd99728395b671618d15d8285299181
tsawant635 <101405324+tsawant635@users.noreply.github.com>
2022-09-23 18:32:25 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
-const JWTPRIVATEKEY = "tsawant635";
+const JWTPRIVATEKEY ="tsawant635";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -41,28 +41,33 @@ const UserSchema = new mongoose.Schema({
 });
 
 
+// jwt token
 
 UserSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, JWTPRIVATEKEY, {
-    expiresIn: "7d",
-  });
-  return token;
+	const token = jwt.sign({ _id: this._id }, JWTPRIVATEKEY, {
+		expiresIn: "7d",
+	});
+	return token;
 };
 
-const UserData = mongoose.model("LaundryUsers", UserSchema);
+const UserData = new mongoose.model("LaundryUsers", UserSchema);
+
+
+// validate part
 
 const validate = (data) => {
-  const schema = Joi.object({
-    name: Joi.string().required().label("Name"),
-    email: Joi.string().email().required().label("Email"),
-    password: passwordComplexity().required().label("Password"),
-    phone: Joi.number().required().label("Phone"),
-    state: Joi.string().required().label("State"),
-    district: Joi.string().required().label("District"),
-    address: Joi.string().required().label("Address"),
-    pincode: Joi.number().required().label("Pincode"),
-  });
-  return schema.validate(data);
+	const schema = Joi.object({
+		name: Joi.string().required().label("Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+        phone: Joi.number().required().label("Phone"),
+        state: Joi.string().required().label("State"),
+        district: Joi.string().required().label("District"),
+        address: Joi.string().required().label("Address"),
+        pincode: Joi.number().required().label("Pincode"),
+	});
+	return schema.validate(data);
 };
 
 module.exports = { UserData, validate };
+
