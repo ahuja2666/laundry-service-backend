be85efac535fe9b16bcc5e3d532362b1853c81ba
tsawant635 <101405324+tsawant635@users.noreply.github.com>
2022-09-23 18:36:27 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,37 +5,47 @@ const Joi = require("joi");
 
 router.post("/", async (req, res) => {
 	try {
-		const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
-
-		const user = await UserData.findOne({ email: req.body.email });
+		// const { error } = validate(req.body);
+		// if (error)
+		// 	return res.status(400).send({ message: error.details[0].message });
+        
+	    //
+        const {emailphone,password} = req.body;
+		   
+		if(isNaN(parseFloat(emailphone))){
+			
+			var user = await UserData.findOne({ email: emailphone });
 		if (!user)
-			return res.status(401).send({ message: "Invalid Email or Password" });
+			return res.status(401).send({ message: "Invalid Email " });
+		}
+		else{
+			
+			var  user = await UserData.findOne({ phone: emailphone });
+		if (!user)
+			return res.status(401).send({ message: "Invalid Phone" });
+		}
+
+		
+		const username=user.name
 
 		const validPassword = await bcrypt.compare(
 			req.body.password,
 			user.password
 		);
 		if (!validPassword)
-			return res.status(401).send({ message: "Invalid Email or Password" });
+			return res.status(401).send({ message: "Invalid Password" });
 
-
+         
 		const token = user.generateAuthToken();
-        
-		res.status(200).send({ data: token, message: "logged in successfully" });
+       
+		res.status(200).send({ data: [username,token] , message: "logged in successfully" });
        
 	} catch (error) {
+		console.log(error)
 		res.status(500).send({ message: "Internal Server Error" });
 	}
 });
 
-const validate = (data) => {
-	const schema = Joi.object({
-		email: Joi.string().email().required().label("Email"),
-		password: Joi.string().required().label("Password"),
-	});
-	return schema.validate(data);
-};
+
 
 module.exports = router;
